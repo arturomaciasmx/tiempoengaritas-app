c14116988323da7b3e62acec1f4962e50696ffb2
Arturo Macias <arturomaciasmx@gmail.com>
2022-02-23 22:20:38 -0800

    add all cities to list from api

--- components/CitiesList.js
+++ components/CitiesList.js
@@ -3,43 +3,45 @@ import { View, ScrollView, TouchableOpacity, Text} from 'react-native'
 
 const CitiesList = ({setCity, toggleOverlay}) => {
 
-  // const [cities, setCities] = useState([])
+  const [cities, setCities] = useState([])
 
-  // const getCities = () => {
-  //   return fetch("https://tiempoengaritas.herokuapp.com/api/cities")
-  //     .then((response) => response.json())
-  //     .then((json) => {
-  //       setCities(json);
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // };
+  const getCities = () => {
+    return fetch("https://tiempoengaritas.herokuapp.com/api/cities")
+      .then((response) => response.json())
+      .then((json) => {
+        setCities(json);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-  // useEffect(() => {
-  //   getCities()
-  //   console.log(cities);
-  // }, [])
-
-  const cities = [{
-    "estado": "Baja California",
-    "ciudades": ["Tijuana", "Tecate", "Mexicali"]
-  }]
+  useEffect(() => {
+    getCities()
+  }, [])
 
   const Ciudad = (props) => {
     return  props.city.ciudades.map((ciudad, index) => (
-              <Text>{ciudad}</Text>
+              <TouchableOpacity 
+                key={index}
+                onPress={() => {
+                  console.log(ciudad);
+                  setCity(ciudad.toLowerCase())
+                  toggleOverlay()
+                }}>
+                <Text style={{ fontSize: 18, marginBottom: 15, color: '#f7f7f7' }}>{ciudad}</Text>
+              </TouchableOpacity>
             ))
   }
 
   return (
-    <View style={{ height: 400, width: 300 }}>
+    <View style={{ height: 600, width: 300, padding: 15 }}>
       <ScrollView>
         {
           cities.map((city, index) => (
-            <View>
-            <Text>{city.estado}</Text>
-             <Ciudad city={city}/>
+            <View key={index}>
+              <Text style={{ fontSize: 23, fontWeight: 'bold', marginBottom: 15 }}>{city.estado}</Text>
+              <Ciudad city={city}/>
             </View>
           ))
         }
